b7b6b9012fcac8e9863592860ea87d87187321eb
beulah703 <68689616+beulah703@users.noreply.github.com>
2021-06-05 15:30:53 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing Bootcamp.Our Mentor Shuarya Sinha taught us so
+        well.We were so benefitted.I would definitely recommend it to all of my
+        friends.{" "}
+      </p>
+      <p>
+        We got to learn about react.js, a little bit of html as well as
+        JavaScript.Overall the camp was awesome.Thank you ShapeAI and Shuarya
+        Sinha sir.{" "}
+      </p>
+    </div>
+  );
+}
+export default Note;
